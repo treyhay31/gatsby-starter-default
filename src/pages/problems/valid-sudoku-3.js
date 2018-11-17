cfea06e11aa451c44c6f4c195e0bf21041d37e9f
Trey Hayden <treyhay@gmail.com>
2018-11-17 11:57:32 -0500

    outlining sudoku pt 3

--- src/pages/problems/valid-sudoku-3.js
+++ src/pages/problems/valid-sudoku-3.js
@@ -4,7 +4,13 @@ import Link from 'gatsby-link'
 const ValidSudoku3Page = () => (
 	<div>
 			<h1>Valid Sudoku - P3</h1>
-			<p>Coming soon! Hoping to <a href="https://www.jamesshore.com/Blog/Red-Green-Refactor.html">refactor</a> all of it!</p>
+			<p>Coming soon! Generating sudoku boards determining if they are valid!</p>
+			<h2>Pull in Sudoku Games</h2>
+			<h2>Transform Game to Expected Input</h2>
+			<h2>Create Tests For It!</h2>
+			<h2>Test It!</h2>
+			<h2>Run It!</h2>
+			<h2>Conclusion</h2>
 			<h2>
 				<Link to="/problems/valid-sudoku">Valid Sudoku</Link>
 				{" | "}
